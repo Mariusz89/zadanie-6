35a14841090773c221de58487cfb417dc71a1a5c
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-08-26 23:41:16 +0200

    sdns

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var a= prompt("give base a");
+var h= prompt("give height h");
+var triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
